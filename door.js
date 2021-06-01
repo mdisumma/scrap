e8bf25a76573c69de5a8cd3a9046528200d7b5d3
Marco Di Summa <mdisumma@yahoo.com>
2021-06-01 14:44:25 +0100

    min

--- door.js
+++ door.js
@@ -3,11 +3,11 @@ const doorRight = document.querySelector(".right");
 const click = document.querySelector("body");
 
 click.addEventListener("click", (e) => {
-	console.log(e);
 	doorControl();
 });
 
 function doorControl() {
+	console.log(doorLeft);
 	doorLeft.classList.toggle("left");
 	doorRight.classList.toggle("right");
 }
